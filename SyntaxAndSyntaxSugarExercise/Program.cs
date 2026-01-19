ea9beb9ab675b684c2ad0aab94214f19a6557e43
Briun Greene <harmonickarma789@gmail.com>
2026-01-19 17:49:34 -0500

    Converted to ternary syntax statement

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            Console.WriteLine(answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater or equal to nine");
+                
         }
     }
 }
+
+/*
+ int answer = 4;
+   string response;
+   if (answer < 9) 
+   {
+       response = answer + " is less than nine";
+   }
+   else
+   {
+       response = answer + " is greater than or equal to nine";
+   }
+ Inferred Typing: Replace explicit types with var.
+   String Interpolation: Refactor the concatenation to use string interpolation.
+   Ternary Operator: Replace the if-else statement with a ternary operator.*/
